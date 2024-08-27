6a9b776d9667b25fe75ea3f4cc6684b1001ac76b
Diego-Neri <143764971+Diego-Neri@users.noreply.github.com>
2024-08-27 17:12:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using ControleDeContatos.Data;
+using MySql.EntityFrameworkCore.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ControleDeContatos.Repositorio;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("MariaDB");
+
+
+builder.Services.AddDbContext<BancoContext>(options =>
+    options.UseMySQL(connectionString));
+
+builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
