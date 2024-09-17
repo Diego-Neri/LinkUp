8ea05ae9d8fd9ba180f268a39944cd2c155138ae
Diego-Neri <143764971+Diego-Neri@users.noreply.github.com>
2024-09-17 15:36:33 -0300

    feat: Adiciona na interface o BuscarPorEmailELogin e AlterarSenha.

--- Repositorio/IUsuarioRepositorio.cs
+++ Repositorio/IUsuarioRepositorio.cs
@@ -8,11 +8,13 @@ using System.Threading.Tasks;
 namespace ControleDeContatos.Repositorio {
     public interface IUsuarioRepositorio {
         UsuarioModel BuscarPorLogin(string login);
+        UsuarioModel BuscarPorEmailELogin(string email, string login);
         UsuarioModel ListarPorId(int id);
         List<UsuarioModel> BuscarTodos();
         UsuarioModel Adicionar(UsuarioModel usuario);
         UsuarioModel Atualizar(UsuarioModel usuario);
 
+        UsuarioModel AlterarSenha(AlterarSenhaModel alterarSenha);
         bool Apagar(int id);
 
     }
